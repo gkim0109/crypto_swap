2229be20638ec1ba28ab28302cdf92b496a76edc
Kim <andykim@deloitte.com>
2017-07-02 09:43:30 -0400

    Removed confidential information

--- SmsNotification/txtTest.py
+++ SmsNotification/txtTest.py
@@ -6,8 +6,8 @@ class twilioAPI():
 
     def sendMessage(self):
         # Find these values at https://twilio.com/user/account
-        account_sid = "AC6cfe9b65c1fc842e655d440b10f3225f"
-        auth_token = "a4784e174c095606ea69d4b2c009db99"
+        account_sid = ""
+        auth_token = ""
         client = Client(account_sid, auth_token)
 
         message = client.api.account.messages.create(to="+19405942980",
